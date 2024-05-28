f901147f801cc32fbfa19c27ea7acd6d0bd74c89
Chandan Kumar Shaw <chandan.kumar.shaw@sap.com>
2024-05-28 20:51:37 +0530

    Refactor

--- client/src/components/Posts/posts.js
+++ client/src/components/Posts/posts.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import Postcard from './postcard';
-import Loading from '../loading/loading';
 
 const  Posts = ({updateId})=> {
   var posts = null;
